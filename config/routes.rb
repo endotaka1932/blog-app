c32dd85ffa8036488c309781fa1ab8726e05d719
endotaka1932 <83772497+endotaka1932@users.noreply.github.com>
2021-12-30 01:05:48 +0900

    Update routes.rb
    修正取り消し

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to　: 'articles#index'
+  root to: 'articles#index'
   resource :timeline, only: [:show]  #[:show, :create] => %i(show create)
 
   resources :articles do
