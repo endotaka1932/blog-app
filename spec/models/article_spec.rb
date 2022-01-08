1488a83ea16680e7d44a322a2b2eac2d04bdfd96
遠藤貴文 <>
2022-01-08 22:09:45 +0900

    rspecのルールを覚えよう 実際にテストを書く

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,4 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
+  context 'タイトルと内容が入力されていれば' do
+    let!(:user) do
+      user = User.create!({
+        email: 'test@example.com',
+        password: 'password'
+      })
+    end
+
+    let!(:article) do
+      article = user.articles.build({
+        title: Faker::Lorem.characters(number: 10),
+        content: Faker::Lorem.characters(number: 300)
+      })
+    end
+
+    it '記事を保存できる' do
+      expect(article).to be_valid
+    end
+  end
+
 end
